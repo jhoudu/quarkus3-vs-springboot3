6c1029374b861fed4564308316ce98b5a07627f7
jhoudu <jhoudu@gmail.com>
2023-05-26 13:19:05 +0200

    upload du projet

---
+++ springboot3/service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+delete from customer;
+insert into customer (name) values  ('Josh');
+insert into customer (name) values  ('Dr. Syer');
+insert into customer (name) values  ('Phil');
+insert into customer (name) values  ('Olga');
+insert into customer (name) values  ('Madhura');
+insert into customer (name) values  ('Violetta');
+insert into customer (name) values  ('Yuxin');
+insert into customer (name) values  ('Stéphane');
