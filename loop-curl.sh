2fdbd0d8086b21d9f1b8b970c2f39542b9f0beea
unknown <jhoudu@gmail.com>
2023-05-29 12:30:48 +0200

    Suppression des scripts de lancement

--- loop-curl.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/customers/Josh)" != "200" ]]; do sleep .00001; done'
--- quarkus3/service/run-quarkus-native.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run -ti --rm -p 8080:8080 quarkus/service-micro
